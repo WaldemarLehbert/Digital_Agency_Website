a329b22f948416f38beca4d839f5afd9a47e254b
wowa512 <waldemarlehbert@web.de>
2022-08-05 22:37:45 +0200

    Added some css for the Header component.

--- digital-agency-website/src/Components/Header.js
+++ digital-agency-website/src/Components/Header.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div>
-            <img src={require("../Images/logo_enver.png")} alt="Ico"/>
-            <p>Enver</p>
-            <nav className="navbar">
+        <div className="header-container">
+            <div className="header--logo">
+                <img src={require("../Images/logo_enver.png")} alt="Ico"/>
+                <p>Enver</p>
+            </div>
+            <nav className="header--navbar">
                 <p>Home</p>
                 <p>Services</p>
                 <p>Our Project</p>
